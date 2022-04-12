946ea157bb3403527128a6d7713f0d41dc38bca2
Evan <djevann11@gmail.com>
2022-04-12 08:41:57 +0700

    initial commit

---
+++ app/src/main/java/com/djevannn/nftmarketplace/ui/add/AddViewModel.kt
@@ -0,0 +1,13 @@
+package com.djevannn.nftmarketplace.ui.add
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AddViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
